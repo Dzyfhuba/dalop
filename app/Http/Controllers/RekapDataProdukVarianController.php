4f69cad11d29fc547aef49c493f27e3007eb036b
Ahmad Farid <ahmadfarid.gresik@gmail.com>
2022-04-07 22:15:15 +0700

    add controller

--- app/Http/Controllers/RekapDataProdukVarianController.php
+++ app/Http/Controllers/RekapDataProdukVarianController.php
@@ -3,28 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\DataProdukHarian;
-<<<<<<< Updated upstream
-use App\Models\Produk;
-use App\Models\ProdukVarian;
-=======
 // use App\Models\Produk;
->>>>>>> Stashed changes
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class RekapDataProdukController extends Controller
+class RekapDataProdukVarianController extends Controller
 {
-    private function get_by_ym($data)
-    {
-
-        $d = [];
-    }
     public function index(Request $request)
     {
-        // dd($request->all());
-        // dd('ad');
-        //SELECT SUM(nilai_realisasi),EXTRACT(MONTH FROM "2021-01-01") FROM data_produk_harians as a INNER JOIN produk_varians as b ON a.id_produk_varian=b.id INNER JOIN produks as c ON b.id_produk = c.id WHERE c.id = 1 AND b.id = 2;
-        
 
         $n = [];
 
@@ -58,13 +44,13 @@ class RekapDataProdukController extends Controller
             ->join('produk_varians', 'data_produk_harians.id_produk_varian', '=', 'produk_varians.id')
             ->join('produks', 'produk_varians.id_produk', '=', 'produks.id')
             ->select(
-                'data_produk_harians.id as id',
+                'produk_varians.nama_produk_varian as produk_varians',
                 'produks.nama_produk as produk',
                 'date',
                 DB::raw('SUM(data_produk_harians.nilai_rencana) as nilai_rencana'),
                 DB::raw('SUM(data_produk_harians.nilai_realisasi) as nilai_realisasi')
             )
-            ->groupBy('produk')
+            ->groupBy('produk_varians')
             ->get();
 
 
@@ -72,17 +58,17 @@ class RekapDataProdukController extends Controller
             ->join('produk_varians', 'data_produk_harians.id_produk_varian', '=', 'produk_varians.id')
             ->join('produks', 'produk_varians.id_produk', '=', 'produks.id')
             ->select(
-                'data_produk_harians.id as id',
+                'produk_varians.nama_produk_varian as produk_varians',
                 'produks.nama_produk as produk',
                 'date',
                 DB::raw('SUM(data_produk_harians.nilai_rencana) as nilai_rencana'),
                 DB::raw('SUM(data_produk_harians.nilai_realisasi) as nilai_realisasi')
             )
-            ->groupBy('produk')
+            ->groupBy('produk_varians')
             ->get();
 
         // dd($data_produk_bulanan);
 
-        return view('rekap_data.index', compact('data_produk_harian', 'date','data_produk_bulanan','first_date','end_date'));
+        return view('rekap_data_varian.index', compact('data_produk_harian', 'date','data_produk_bulanan','first_date','end_date'));
     }
 }
